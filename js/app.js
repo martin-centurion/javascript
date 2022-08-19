74aa6027174b17c44dd737750f939a2befd391c2
martin-centurion <martinalejandrocenturion@gmail.com>
2022-08-18 23:55:50 -0300

    agregando while, prompt y for al js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
-let acierto = false;
-let contador = 0;
-let respuesta = '';
+// while
 
-while (contador < 3 && acierto === false) {
-    let respuestaUsuario = prompt('Capital de Canada');
-    if (respuestaUsuario === respuesta) {
-        acierto = true;
-        alert('Acertaste');
+let edad = parseInt (prompt('Ingresa tu edad'));
+
+while (edad < 18) {
+    console.log('No puedes ingresar, eres menor de 18');
+    edad = parseInt(prompt('Ingresa tu edad'));
+}
+
+// Prompt - Multiplicacion
+
+let valor = parseInt(prompt('Ingrese un numero'));
+let multiplo = parseInt(prompt('Ingresa un limite'));
+
+for (let index = 0; index < multiplo; index++) {
+    let resultado = valor * 1;
+    console.log(valor + ' * ' + index + ' = ' + resultado);
+}
+
+
+// For
+let palabraClave = 'cafe';
+
+for (let index = 1; index <= 3; index++){
+    let palabraUsuario = prompt ('Ingrese su Password');
+    if (palabraClave === palabraUsuario) {
+        console.log('Respuesta Correcta');
+        break;
+    }else{
+        console.log('Te quedan ' + (3 - index) + ' intentos');
     }
-    contador++;
+    if (index == 3) {
+        console.log('Actualiza para intentarlo de nuevo :(');
+    }
 }
