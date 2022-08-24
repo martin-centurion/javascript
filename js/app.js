9f09e6c332bd3cf84f77fb16273c7e3a04a613ff
martin-centurion <martinalejandrocenturion@gmail.com>
2022-08-24 19:31:15 -0300

    agregando page pruebas

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
 
 let ingresarNombre = prompt('Ingrese su nombre');
+
+while (ingresarNombre == '') {
+    alert('Error: Por favor, ingrese su nombre');
+    ingresarNombre = prompt('Ingrese su nombre');
+}
+
 let ingresarApellido = prompt('Ingrese su apellido');
 
-
-while ((ingresarNombre !== ' ') && (ingresarApellido !== ' ')) {
-    alert('Error: ingrese su nombre y apellido');
-    ingresarNombre = prompt('Ingrese su nombre');
-    ingresarApellido = prompt ('Ingrese su apellido');
+while (ingresarApellido == '') {
+    alert('Error: Por favor, ingrese su apellido');
+    ingresarApellido = prompt('Ingrese su apellido');
 }
 
 alert('Bienvenido/a ' + ingresarNombre + ' ' + ingresarApellido);
 
-let preguntas = false;
+let bienvenido = false;
 do {
-    alert(ingresarNombre + ', contesta las siguientes preguntas, por favor')
-} while (preguntas);
-
-for (let index = 1; index <= 3; index++) {
-    let ingresarClub = prompt('De que club eres hincha?');
-    let ingresarColor = prompt('Cual es tu color preferido?');
-    alert('Gracias por responder, sos hincha de ' + ingresarClub + ' y tu color preferido es ' + ingresarColor );
-    if ((ingresarClub !== '') && (ingresarColor !== '')) {
-        break;
-    } else {
-        alert('Conteste las preguntas por favor')
-    }
+    alert(ingresarNombre + ' ' + ingresarApellido + ', contesta las siguientes preguntas, por favor');
+} while (bienvenido) {
+    
 }
+
+let ingresarClub = prompt('De que club eres hincha?');
+let ingresarColor = prompt('Cual es tu color preferido?');
+
+while (ingresarClub == '' && ingresarColor == '') {
+    alert('Error: Conteste las preguntas, por favor');
+    ingresarClub = prompt('De que club eres hincha?');
+    ingresarColor = prompt('Cual es tu color preferido?');
+}
+
+alert('Gracias por responder, sos hincha de ' + ingresarClub + ' y tu color preferido es ' + ingresarColor );
+
+
+
 
 let palabraClave = 'cafe';
 
---
+++ js/prueba.js
@@ -0,0 +1,6 @@
+let numero = 0;
+
+do {
+    numero = prompt('Ingresar numero');
+    alert(numero);
+} while (parseInt(numero));
